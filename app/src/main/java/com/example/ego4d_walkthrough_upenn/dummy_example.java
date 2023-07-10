e14ce5fe52ab920617717dbc20505c455aa6d9ad
zedward23 <67485515+zedward23@users.noreply.github.com>
2023-07-10 13:25:57 -0400

    added dummy example

---
+++ app/src/main/java/com/example/ego4d_walkthrough_upenn/dummy_example.java
@@ -0,0 +1,14 @@
+package com.example.ego4d_walkthrough_upenn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dummy_example extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_example);
+    }
+}
